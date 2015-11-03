2dd66dc4f1fe5ac5593bf480b44ec9f5cdc7a6c7
garaytl <lea.garayt@gmail.com>
2015-11-03 08:35:18 +0100

    aUpdateItemDecreaseByOneQualityAndSellIn()

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,14 +2,22 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class GildedRoseTest
 {
 	@Test
-	public void aSimpleBeginningTest()
+	public void aUpdateItemDecreaseByOneQualityAndSellIn()
 	{
-		assertEquals(true,false);
+		GildedRose myShop = new GildedRose();
+		Item vestSwag = new Item("+5 Dexterity Vest", 10, 20);
+		myShop.updateItem(vestSwag);
+		assertEquals(vestSwag.getQuality(),19);
+		assertEquals(vestSwag.getSellIn(),9);
+		
 	}
+	
 
 }
